4fa740d2508cbadf3b9a7fc63a2eee521fad1cca
jakelig <43222956+jakelig@users.noreply.github.com>
2019-04-03 14:47:28 -0400

    CU Logo moves with camera
    - CU Logo now moves with the camera
    - Changed the y position of the original logo
    - Changed the logo from a sphere to a circle

--- components/set-camera.js
+++ components/set-camera.js
@@ -4,11 +4,13 @@ AFRAME.registerComponent('set-camera', {
         // First we grab the new position from the HTML code
         el.addEventListener('click', function () {
             var camera = document.querySelector('#Camera'),
+                paw = document.querySelector('#paw'),
                 newX = el.getAttribute('data-newX'),
                 newY = el.getAttribute('data-newY'),
                 newZ = el.getAttribute('data-newZ');
             // Then we change the camera's position to these values
             camera.setAttribute('position', {x: newX, y: newY, z: newZ});
+            paw.setAttribute('position', {x: newX, y: newY - 4, z: newZ});
         });
     }
 });    
